bfbe7acdc571164ca20e9767ba3548d48dac8b1f
Maksim Rudenko <rudenko.programmer@gmail.com>
2019-04-10 02:13:07 +0300

    Init package

---
+++ src/GetResponseServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace RudenkoProgrammer\GetResponseLaravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class GetResponseServiceProvider extends ServiceProvider
+{
+
+	/**
+	 * Register services.
+	 *
+	 * @return void
+	 */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
